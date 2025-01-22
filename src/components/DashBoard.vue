119489240ca605aba09bc8bb115145b9dd44a17a
Sherlockespitia <claw.1991@gmail.com>
2025-01-22 16:53:55 -0500

    Render menu from data response

--- src/components/DashBoard.vue
+++ src/components/DashBoard.vue
@@ -1,12 +1,42 @@
 <template>
-  <h1>
-    Dashboard
-  </h1>
+  <BNavbar class="navbar navbar-light bg-light d-flex wrap ">
+    <BNavbarBrand>{{ user }}</BNavbarBrand>
+    <BNavbarNav >
+      <BNavItemDropdown v-for="menu in menuOpciones" :key="menu.idModulo" :text="menu.nombre">{{ menu.id }}</BNavItemDropdown>
+    </BNavbarNav>
+  </BNavbar>
+  <BContainer>The Container </BContainer>
 </template>
 
 <script>
-export default {
 
+export default {
+  data(){
+    return {
+      dataMenu: JSON.parse(localStorage.getItem('dataMenu')),
+    }
+  },
+  created(){
+    console.log('dataMenu', this.dataMenu)
+  },
+  computed:{
+    user(){
+      let data = JSON.parse(localStorage.getItem('data'))
+      if(data){
+        return data.nombre
+      }
+      return 'user'
+    },
+    menuOpciones(){
+      let {menu} = this.dataMenu
+      if(menu){
+        console.log('menu', menu)
+        return menu
+
+      }
+      return []
+    }
+  }
 }
 </script>
 
